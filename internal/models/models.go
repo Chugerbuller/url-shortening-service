646981a2129b796b4e450b2b8278f22e28934ded
chuga <sprunk59@yandex.ru>
2025-11-22 22:26:35 +0500

    start-storage

--- internal/models/models.go
+++ internal/models/models.go
@@ -5,5 +5,5 @@ type Url struct {
 	Url       string `db:"url"`
 	ShortUrl  string `db:"short_url"`
 	CreatedAt int    `db:"created_at"`
-	UpdatedAt int    `db:"updated_at"`
+	UpdatedAt int64  `db:"updated_at"`
 }
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -2,8 +2,9 @@ package storage
 
 import (
 	"database/sql"
+	"time"
 
-	"github.com/Chugerbuller/url-shortening-service/internal/models"
+	"url-shortening-service/internal/models"
 )
 
 type Storage struct {
@@ -13,13 +14,13 @@ type Storage struct {
 func NewStorage(db *sql.DB) *Storage {
 	return &Storage{db: db}
 }
-func (s *Storage) Add(url models.Url) error {
+func (s *Storage) Add(url models.Url) (int, error) {
 	query := "INSERT INTO urls (url, short_url, created_at, updated_at) VALUES (:url, :short_url, :created_at, :updated_at)"
 	res, err := s.db.Exec(query,
 		sql.Named("url", url.Url),
 		sql.Named("short_url", url.ShortUrl),
-		sql.Named("address", url.CreatedAt),
-		sql.Named("created_at", url.UpdatedAt),
+		sql.Named("created_at", url.CreatedAt),
+		sql.Named("updated_at", url.UpdatedAt),
 	)
 	if err != nil {
 		return 0, err
@@ -31,4 +32,33 @@ func (s *Storage) Add(url models.Url) error {
 	}
 
 	return int(lastID), nil
+}
+func (s *Storage) GetByShortUrl(shortUrl string) (*models.Url, error) {
+	var url models.Url
+	query := "SELECT id, url, short_url, created_at, updated_at FROM urls WHERE short_url = :short_url"
+	row := s.db.QueryRow(query, sql.Named("short_url", shortUrl))
+	err := row.Scan(&url.ID, &url.Url, &url.ShortUrl, &url.CreatedAt, &url.UpdatedAt)
+
+	if err != nil {
+		return nil, err
+	}
+	return &url, nil
+}
+func (s *Storage) Update(shortUrl, newUrl string) (*models.Url, error) {
+	var updatedUrl models.Url
+
+	query := "UPDATE urls SET url = :newUrl,updated_at = :updated_at WHERE short_url = :shortUrl RETURNING id, url, short_url, created_at, updated_at"
+	row, err := s.db.Query(query,
+		sql.Named("newUrl", newUrl),
+		sql.Named("updated_at", time.Now().Unix()),
+		sql.Named("shortUrl", shortUrl),
+	)
+	if err != nil {
+		return nil, err
+	}
+	err = row.Scan(&updatedUrl.ID, &updatedUrl.Url, &updatedUrl.ShortUrl, &updatedUrl.CreatedAt, &updatedUrl.UpdatedAt)
+	if err != nil {
+		return nil, err
+	}
+	return &updatedUrl, nil
 }
